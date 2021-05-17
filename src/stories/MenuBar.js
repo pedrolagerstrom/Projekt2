18cc456482d91333ea24de59e6ffe245ab156f8e
Pedro Lagerström <pedro.lagerstrom@student.kyh.se>
2021-05-17 15:47:24 +0200

    Lagt till swagger, fixat menubar iconer

--- src/stories/MenuBar.js
+++ src/stories/MenuBar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { ReactComponent as UserIcon } from "./icons/user.svg";
-import { ReactComponent as SkisIcon } from "./icons/skis.svg";
-import { ReactComponent as MapPinIcon } from "./icons/map-pin.svg";
-import { ReactComponent as CalendarIcon } from "./icons/calendar.svg";
-import { ReactComponent as SettingsIcon } from "./icons/settings.svg";
+import UserIcon from "./icons/user";
+import SkisIcon from "./icons/skis";
+import MapPinIcon from "./icons/map-pin";
+import CalendarIcon from "./icons/calendar";
+import SettingsIcon from "./icons/settings";
 
 const Container = {
   display: "flex",
@@ -13,47 +13,23 @@ const Container = {
   height: "25px",
   //backgroundColor: "black",
 };
-const User = {
-  src: UserIcon,
-  color: "black",
-};
-
-const Skis = {
-  src: SkisIcon,
-  color: "black",
-};
-
-const MapPin = {
-  src: MapPinIcon,
-  color: "black",
-};
-
-const Calendar = {
-  src: CalendarIcon,
-  color: "black",
-};
-
-const Settings = {
-  src: SettingsIcon,
-  color: "black",
-};
 
 const MenuBar = () => (
   <div style={Container}>
     <div style={{ margin: "30px" }}>
-      <img src={User.src}></img>
+      <UserIcon />
     </div>
     <div style={{ margin: "30px" }}>
-      <img src={Skis.src}></img>
+      <SkisIcon />
     </div>
     <div style={{ margin: "30px" }}>
-      <img src={MapPin.src}></img>
+      <MapPinIcon />
     </div>
     <div style={{ margin: "30px" }}>
-      <img src={Calendar.src}></img>
+      <CalendarIcon />
     </div>
     <div style={{ margin: "30px" }}>
-      <img src={Settings.src}></img>
+      <SettingsIcon />
     </div>
   </div>
 );
